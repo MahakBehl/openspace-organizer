4b1a6844c682ce98bdc023a50bb7b8ecc909b58f
Mahak Behl <mahakbehl@gmail.com>
2024-01-25 09:59:36 +0100

    first commit

---
+++ src/table.py
@@ -0,0 +1,17 @@
+class Seat:
+    """
+    """
+    def __init__(self,free,occupant):
+        self.free = free
+        self.occupant = occupant
+
+    def set_occupant(name):
+        if self.free:
+            """check seat is free and assign someone to seat"""
+    
+    def remove_occupant():
+        """removes someone from a seat and returns the name of the person occupying the seat before"""
+
+
+class Table:
+    
